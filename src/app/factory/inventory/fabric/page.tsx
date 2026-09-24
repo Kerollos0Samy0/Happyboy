7e38d35a0cb65ee901dc23d32e88e19ae54ec16c
Kerollos0Samy0 <kokosamy1111@gmail.com>
2026-09-24 15:58:13 +0300

    Make barcode text bold

--- src/app/factory/inventory/fabric/page.tsx
+++ src/app/factory/inventory/fabric/page.tsx
@@ -665,7 +665,7 @@ export default function FabricInventoryPage() {
                     <span className="font-black text-[12px] leading-tight text-black">{roll.amount} {roll.unit}</span>
                   </div>
                   <div className="flex items-center justify-center w-full bg-white rounded flex-1 pb-1">
-                    <Barcode value={roll.code} width={1.8} height={26} fontSize={24} margin={0} textMargin={2} textMargin={2} displayValue={true} background="#ffffff" />
+                    <Barcode value={roll.code} width={1.8} height={26} fontSize={24} fontOptions="bold" margin={0} textMargin={2} textMargin={2} displayValue={true} background="#ffffff" />
                   </div>
                 </div>
               ))}
